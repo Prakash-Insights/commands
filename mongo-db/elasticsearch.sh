ec9749b3c0b671a18b044b51f1aebfaaa9ca8c3d
Prakash-Insights <39665531+Prakash-Insights@users.noreply.github.com>
2023-10-16 13:39:24 +0530

    Update elasticsearch.sh

--- mongo-db/elasticsearch.sh
+++ mongo-db/elasticsearch.sh
@@ -1,4 +1,7 @@
 # how-to-install-and-configure-elasticsearch-on-ubuntu-20-04
+# URL : https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-elasticsearch-on-ubuntu-20-04#step-4-testing-elasticsearch
+
+
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
