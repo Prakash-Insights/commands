f47cd0235d3af99e478250a17fd11b0256c6ea03
Prakash-Insights <39665531+Prakash-Insights@users.noreply.github.com>
2023-10-18 20:02:06 +0530

    Update atlas-search.sh

--- mongo-db/atlas-search.sh
+++ mongo-db/atlas-search.sh
@@ -6,3 +6,6 @@ sudo apt-get install -y mongodb-atlas
 sudo apt-get install -y mongodb-atlas-cli
 #Run the atlas command from any directory:
 atlas --version
+# https://www.mongodb.com/community/forums/t/atlas-search-local-development/606/3
+# There currently isn’t an on-prem equivalent of Atlas Search, so the solutions are also unchanged: use an Atlas cluster
+# (Atlas Search is now available on all clusters tiers running MongoDB 4.2 or later) or implement a fallback query for local testing.
